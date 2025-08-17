500edd4c8288fd5cdeeb685c6941b9b2f3c368e8
Ifeanyijosephh <josephify304@gmail.com>
2025-08-17 13:14:49 +0100

    adding new logic in the emailbackend JS file so the submit button clears the input fields after submission

--- emailbackend.js
+++ emailbackend.js
@@ -1,9 +1,19 @@
+ 
+const userName = document.getElementById("name");
+const userEmail = document.getElementById("email");
+const userMessage = document.getElementById("textarea");
+const submitButton = document.getElementById("submitButton");
 
-document.getElementById("contactForm").addEventListener("submit", function (event) {
+document.getElementById("contactForm").addEventListener("submit", function emailSending(event) {
+    if(emailjs.send("service_nkzwax4", "template_r9khy9e", contactForm, "VKZbfOKBqBLgiCSy3")) {
+        userName.value = "";
+        userEmail.value = "";
+        userMessage.value = "";
+    }
      event.preventDefault();
         emailjs.send("service_nkzwax4", "template_r9khy9e", contactForm, "VKZbfOKBqBLgiCSy3")
     .then(() => {
-        showSuccessMessage();
+        alert("Your Message was successfully sent!");
             }, (error) => {
         alert("Failed to send!: " + JSON.stringify(error));
     });
