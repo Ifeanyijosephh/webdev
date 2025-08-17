e721ea3fb555c98a308a1badd47062c3a61b34b5
Ifeanyijosephh <josephify304@gmail.com>
2025-08-17 15:53:38 +0100

    removed the script for email handling and used formspree for simple backend

--- emailbackend.js
+++ emailbackend.js
@@ -1,15 +1,15 @@
- 
+ /*
 const userName = document.getElementById("name");
 const userEmail = document.getElementById("email");
 const userMessage = document.getElementById("textarea");
-const submitButton = document.getElementById("submitButton");
+const submitButton = document.getElementById("submitButton"); */
 
 document.getElementById("contactForm").addEventListener("submit", function emailSending(event) {
-    if(emailjs.send("service_nkzwax4", "template_r9khy9e", contactForm, "VKZbfOKBqBLgiCSy3")) {
-        userName.value = "";
-        userEmail.value = "";
-        userMessage.value = "";
-    }
+   // if(emailjs.send("service_nkzwax4", "template_r9khy9e", contactForm, "VKZbfOKBqBLgiCSy3")) {
+     //   userName.value = "";
+       // userEmail.value = "";
+       // userMessage.value = "";
+    // }
      event.preventDefault();
         emailjs.send("service_nkzwax4", "template_r9khy9e", contactForm, "VKZbfOKBqBLgiCSy3")
     .then(() => {
